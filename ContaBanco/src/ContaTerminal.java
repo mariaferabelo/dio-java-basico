209ae86612ddf7010ce517fa924812d0f8fc41dd
mariaferabelo <mariafernandamrabelo@gmail.com>
2025-04-01 21:51:19 -0300

    códgio criado de acordo com instruções

--- ContaBanco/src/ContaTerminal.java
+++ ContaBanco/src/ContaTerminal.java
@@ -1,5 +1,22 @@
+import java.util.Scanner;
+
 public class ContaTerminal {
     public static void main(String[] args) throws Exception {
-        System.out.println("Hello, World!");
+        
+        Scanner scanner = new Scanner (System.in);
+
+        System.out.println("Digite o número da Agência: ");
+        int agencia = scanner.nextInt();
+
+        System.out.println("Digite o número da conta: ");
+        int conta = scanner.nextInt();
+
+        System.out.println("Digite seu nome: ");
+        String nome = scanner.next();
+
+        System.out.println("Digite o seu saldo em reais (R$): ");
+        double saldo = scanner.nextDouble();
+
+        System.out.println("Olá, " + nome + "! Obrigado por criar uma conta em nosso banco! Sua agência é " + agencia + " e o número de sua conta é " + conta + ". Seu saldo de R$" + saldo + " já está disponível para saque.");
     }
 }
